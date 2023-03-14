af7597f3fb5265403b6913c074f6a0f85e88a0e1
Rajendran Madushan <madushanrajendran@gmail.com>
2023-03-14 14:12:37 -0400

    Created object class

---
+++ Product.py
@@ -0,0 +1,52 @@
+class Product:
+
+    def __init__(self,product_code,product_name,sale_price,manufacture_cost,stock_level,units_sold):
+        self.product_code = product_code
+        self.product_name = product_name
+        self.sale_price = sale_price
+        self.manufacture_cost = manufacture_cost
+        self.stock_level = stock_level
+        self.units_sold = units_sold
+
+    def display(self):
+        print(self.product_code,self.product_name,self.sale_price,self.manufacture_cost,self.stock_level,self.units_sold)
+
+
+    def setProduct_code(self,product_code):
+        self.product_code = product_code
+
+    def setProduct_name(self,product_name):
+        self.product_name = product_name
+
+    def setSale_Price(self,sale_price):
+        self.sale_price = sale_price
+
+    def setManufacture_cost(self,manufacture_cost):
+        self.manufacture_cost = manufacture_cost
+
+    def setStock_Level(self,stock_level):
+        self.stock_level = stock_level
+
+    def setUnits_Sold(self,units_sold):
+        self.units_sold = units_sold 
+
+    def getProduct_Code(self):
+        print(self.product_code)
+
+    def getProduct_Name(self):
+        print(self.product_name)
+
+    def getSale_price(self):
+        print(self.sale_price)
+
+    def getManufacture_cost(self):
+        print(self.manufacture_cost)
+
+    def getProduct_Code(self):
+        print(self.stock_level)
+
+    def getProduct_Code(self):
+        print(self.units_sold)
+    
+
+    
