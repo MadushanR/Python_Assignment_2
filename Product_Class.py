4f95d57a2618b3585c2fee305be85158361a414c
Rajendran Madushan <madushanrajendran@gmail.com>
2023-03-17 00:12:11 -0400

    added comments

--- Product_Class.py
+++ Product_Class.py
@@ -1,6 +1,6 @@
-class Product:
+class Product: #creates Product class
 
-    def __init__(self,product_code,product_name,sale_price,manufacture_cost,stock_level,units_produced,units_sold):
+    def __init__(self,product_code,product_name,sale_price,manufacture_cost,stock_level,units_produced,units_sold): #constructor
         self.product_code = product_code
         self.product_name = product_name
         self.sale_price = sale_price
@@ -9,11 +9,11 @@ class Product:
         self.units_produced = units_produced
         self.units_sold = units_sold
 
-    def display(self):
+    def display(self): #displays all variables of the Product class
         print(self.product_code,self.product_name,self.sale_price,self.manufacture_cost,self.stock_level,self.units_sold)
 
 
-    def setProduct_code(self,product_code):
+    def setProduct_code(self,product_code): #setter
         self.product_code = product_code
 
     def setProduct_name(self,product_name):
@@ -34,7 +34,7 @@ class Product:
     def setUnits_Sold(self,units_sold):
         self.units_sold = units_sold 
 
-    def getProduct_Code(self):
+    def getProduct_Code(self): #getter
         return self.product_code
 
     def getProduct_Name(self):
--- application.py
+++ application.py
@@ -1,65 +1,68 @@
-import Product_Class
-import random
+import Product_Class #imports Class Product
+import random #imports random library
 
 print("Welcome to Programming Principles Sample Product Inventory")
-product_code = int(input("Please enter the Product Code : "))
-while product_code<100 or product_code>1000 :
+product_code = int(input("Please enter the Product Code : ")) #gets product code input
+while product_code<100 or product_code>1000 : #validates input
     product_code = int(input("Enter a product code between 100 and 1000 : "))
 
-product_name = input("Please enter the Product Name : ")
+product_name = input("Please enter the Product Name : ") #gets product name input
 
-stock_level = int(input("Please enter the Current Stock Level : "))
-while stock_level < 0 :
+stock_level = int(input("Please enter the Current Stock Level : ")) #gets stock level input
+while stock_level < 0 : #validates input
     stock_level = int(input("Enter stock level greater than 0 : "))
 
-sale_price = float(input("Please enter the Product Sale Price : "))
-while sale_price<0:
+sale_price = float(input("Please enter the Product Sale Price : ")) #gets sale price input
+while sale_price<0: #validates input
     sale_price = float(input("Enter a sale price greater than 0 : "))
 
-manufacture_cost = float(input("Please enter the Manufacture Cost : "))
-while manufacture_cost<0:
+manufacture_cost = float(input("Please enter the Manufacture Cost : ")) #gets manufacture cost input
+while manufacture_cost<0: #validates input
     manufacture_cost = float(input("Enter manufacture cost greater than 0 : "))
 
-units_produced = int(input("Please enter estimated montly production : "))
-while units_produced < 0:
+units_produced = int(input("Please enter estimated montly production : ")) #gets montly production input
+while units_produced < 0: #validates input
     units_produced = int(input("Enter estimated montly production greater than 0 : "))
 
 
-P1 = Product_Class.Product(product_code,product_name,sale_price,manufacture_cost,stock_level,units_produced,0)
+P1 = Product_Class.Product(product_code,product_name,sale_price,manufacture_cost,stock_level,units_produced,0) #calls construct method with given arguments in product class
 
 print("\n********Programming Principles Sample Stock Statement*******")
 
-print("\nProduct Code : " , P1.getProduct_Code())
-print("Product Name : " , P1.getProduct_Name())
-print("\nSale Price : " , P1.getSale_price() , " CAD ")
-print("Manufacture Cost: " , P1.getManufacture_cost() , " CAD")
-print("Monthly Production: " , P1.getUnits_produced() , " units (Approx.) \n")
+print("\nProduct Code : " , P1.getProduct_Code()) #prints product code using get method in product class
+print("Product Name : " , P1.getProduct_Name()) #prints product name using get method in product class 
+print("\nSale Price : " , P1.getSale_price() , " CAD ") #prints sale price using get method in product class
+print("Manufacture Cost: " , P1.getManufacture_cost() , " CAD") #prints manufacture cost using get method in product class
+print("Monthly Production: " , P1.getUnits_produced() , " units (Approx.) \n") #prints units produced using get method in product class
 
-month = 1
-total_sold = 0
-not_sold = []
-while month <13:
+month = 1 #intializes month to 1
+total_sold = 0 #initializes total sold products to 0
+not_sold = [] #initializes a list to store failed sales month due to less stock
+while month <13: #checks if month is less than 13
 
     print("Month ", month , " :")
-    print("|   Manufactured : ", P1.getUnits_produced() , " units")
-    sold = random.randint(int(P1.getUnits_produced())-10,int(P1.getUnits_produced())+10)
-    if (int(P1.getStock_level())+int(P1.getUnits_produced())) >= sold :
-        total_sold = total_sold + sold
-        print("|   Sold : ", sold , " units")
-        stock = int(P1.getStock_level()) + int(P1.getUnits_produced()) - sold
-        P1.setStock_Level(stock)
+    print("|   Manufactured : ", P1.getUnits_produced() , " units") #prints units produced using get method in product class
+    sold = random.randint(int(P1.getUnits_produced())-10,int(P1.getUnits_produced())+10) #generates a random number between +/- 10 of units produced as sales
+    if (int(P1.getStock_level())+int(P1.getUnits_produced())) >= sold : #check if there is enough stock for sale
+        total_sold = total_sold + sold #adds total sold with sold
+        print("|   Sold : ", sold , " units") 
+        stock = int(P1.getStock_level()) + int(P1.getUnits_produced()) - sold #calculates current stock after sales
+        P1.setStock_Level(stock) #stores the currect stock to product object
         print( "|   Stock : ", stock , " units")
     else :
-        stock = int(P1.getStock_level()) + int(P1.getUnits_produced())
-        P1.setStock_Level(stock)
+        stock = int(P1.getStock_level()) + int(P1.getUnits_produced()) #calculates current stock
+        P1.setStock_Level(stock) #stores the currect stock to product object
         print("|   Product required is ", sold , " but stock available is only", stock)
-        not_sold.append(month)
-    month = month + 1
+        not_sold.append(month) #appends the failed sales month list
+    month = month + 1 #increments month
 
-net_profit = (total_sold * int(P1.getSale_price())) - (12 * int(P1.getUnits_produced())* int(P1.getManufacture_cost()))
-print("\n Net Profit : " , net_profit , " CAD")
+net_profit = (total_sold * int(P1.getSale_price())) - (12 * int(P1.getUnits_produced())* int(P1.getManufacture_cost())) #calculates net profit by total sales and total productions
+if net_profit > 0 : #checks if it is profit or loss to display relevant message
+    print("\n Net Profit : " , net_profit , " CAD")
+else :
+    print("\n Net Loss : " , net_profit , " CAD")
 
-if len(not_sold) < 1 :
+if len(not_sold) < 1 : #displays the failed sales month list
     print("\n No failed sales")
 else :
     print("\n Products were not sold in below months due to no stock :")
